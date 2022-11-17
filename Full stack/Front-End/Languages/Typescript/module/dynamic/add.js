26cb7fd9ca3dc616d3280fac2c15bdf964376323
hanu829 <117249907+hanu829@users.noreply.github.com>
2022-11-18 00:32:04 +0530

    mdfnsjk

---
+++ Full stack/Front-End/Languages/Typescript/module/dynamic/add.js
@@ -0,0 +1,1 @@
+export const add=(a,b)=>{return a+b}
--- Full stack/Front-End/Libraries/flipkart/flipkart/src/App.tsx
+++ Full stack/Front-End/Libraries/flipkart/flipkart/src/App.tsx
@@ -1,31 +1,19 @@
-const Home = ()
 
+const Home = () => {return <><h1>hello im home</h1></>}
 
-
-
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export const App = () => {
+  console.log()
+  return (<><Home/><Home/><Home/><Home/><Home/></>
+  )
 }
 
-export default App;
+// function a(){
+
+// }
+// function b(){
+//     a()
+//     <h1>  a()</h1>
+// }
+
+
+
