2758f59096257be8ad3f3e0feef4eba831d1649a
hanu829 <117249907+hanu829@users.noreply.github.com>
2022-11-22 21:07:14 +0530

    sjhdcb

--- Full stack/Front-End/Libraries/flipkart/flipkart/src/App.tsx
+++ Full stack/Front-End/Libraries/flipkart/flipkart/src/App.tsx
@@ -3,7 +3,7 @@ import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
 
 import { Contact, Home } from './pages'
 import { Mymovies } from './pages/mymovies/Mymovies'
-
+import { Jewlry } from './pages/jewellary/Jewellary'
 export const App = () => {
 
     return (<>
@@ -12,8 +12,8 @@ export const App = () => {
        < Route path='/' element={<Home/>}/>
        < Route path='/contact' element={<Contact/>}/>
        < Route path='/about' element={<h1>hello im about</h1>}/>
-       
        < Route path='/about' element={<Mymovies/>}/>
+       < Route path='/jewellary' element={<Jewlry/>}/>
 
         </Routes>
         </Router> 
