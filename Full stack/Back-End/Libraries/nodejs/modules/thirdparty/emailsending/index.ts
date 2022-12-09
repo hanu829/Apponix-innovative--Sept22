11ff6681fcc98ed27f23e48d7e8df9e64c2e4e5e
hanu829 <117249907+hanu829@users.noreply.github.com>
2022-12-09 12:51:37 +0530

    jksak

--- Full stack/Back-End/Libraries/nodejs/modules/thirdparty/emailsending/index.ts
+++ Full stack/Back-End/Libraries/nodejs/modules/thirdparty/emailsending/index.ts
@@ -1,4 +1,21 @@
 var nodemailer=require('nodemailer')
 var transport=nodemailer.createTransport({
     service:'gmail',
-    auth:
+    auth:{
+        user:'hanamantarenad565@gmail.com',
+        pass:'my password'
+    }
+})
+var maildata={
+    from:'hanamantarenad565@gmail.com',
+    to:'daluleelavathi@gmail.com',
+    subject:'testing u r awake',
+text:'hi can u dance'
+}
+transport.sendMail(maildata,(err:any,res:any)=>{
+    if(err){
+        console.log(err)
+    }else{
+        console.log('mail send')
+    }
+})
