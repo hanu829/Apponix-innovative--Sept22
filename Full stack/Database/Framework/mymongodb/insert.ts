1ffe3ba3039f1d303ade4d6310806c55b02a6c4a
hanu829 <117249907+hanu829@users.noreply.github.com>
2022-12-15 00:14:05 +0530

    jksefhaf

--- Full stack/Database/Framework/mymongodb/insert.ts
+++ Full stack/Database/Framework/mymongodb/insert.ts
@@ -4,7 +4,7 @@ MongodbClient.connect(url,(err:any,db:any)=>{
     if(err) throw err
     console.log('db connected')
     var dbo=db.db('testDB')
-    var chandu={name:'schandu',chocalate:'silk',heroin:'ananya'}
+    var chandu={name:'dhanush',chocolate:'5star',heroine:'dipika padukone'}
     dbo.collection('test-student').insertOne(chandu,(err:any,res:any)=>{
         if(err)throw err;
         console.log('inserted data',res)
