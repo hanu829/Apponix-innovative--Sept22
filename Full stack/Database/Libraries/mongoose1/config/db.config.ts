274bb8d697e9e7dd3f586f5814b72a9f04aa0d44
hanu829 <117249907+hanu829@users.noreply.github.com>
2022-12-18 10:51:08 +0530

    message

---
+++ Full stack/Database/Libraries/mongoose1/config/db.config.ts
@@ -0,0 +1,9 @@
+const mongoose=require('mongoose')
+var url="mongodb://127.0.0.1:27017/testDB"
+mongoose.connect(url,(err:any)=>{
+    if(err){
+        console.log(err)
+    }else{
+        console.log('db connected')
+    }
+})
