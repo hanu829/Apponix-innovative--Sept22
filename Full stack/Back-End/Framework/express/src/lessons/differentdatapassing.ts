506a8288e8490a5e1684a2a1bf02a138807aac41
hanu829 <117249907+hanu829@users.noreply.github.com>
2022-12-21 20:54:51 +0530

    three ways of getting user info params-querry-body

---
+++ Full stack/Back-End/Framework/express/src/lessons/differentdatapassing.ts
@@ -0,0 +1,6 @@
+app.get('/',(req:any,res:any)=>{
+res.send([{a:'apple',b:'ball'}])
+})
+app.get('/a',(req:any,res:any)=>{
+    res.send([{a:'apple',b:'ball'}])
+})
---
+++ Full stack/Back-End/Framework/express/src/lessons/middleware.ts
@@ -0,0 +1,7 @@
+function middle(req:any,res:any,next:any){
+    console.log('i m called advanced')
+    next()
+}
+  //calling
+//app.use(middle)
+    ///middel ware we give in in curd methods
