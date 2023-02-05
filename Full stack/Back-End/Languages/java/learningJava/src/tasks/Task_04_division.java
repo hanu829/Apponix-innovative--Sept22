fdafd99968c6353b0070e66bc51de02a0e926cbc
hanu829 <117249907+hanu829@users.noreply.github.com>
2023-02-05 20:04:02 +0530

    sejk

--- Full stack/Back-End/Languages/java/learningJava/src/tasks/Task_04_division.java
+++ Full stack/Back-End/Languages/java/learningJava/src/tasks/Task_04_division.java
@@ -5,7 +5,7 @@ public class Task_04_division {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		  int a=84;
-	      int b=26;
+	      int b=12;
 	      System.out.println(a/b);
 	}
 
